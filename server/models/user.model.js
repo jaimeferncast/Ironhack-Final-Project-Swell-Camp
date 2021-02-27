ae3e01e2f3ad1be070e398994947a5f4acd9d78d
Barbara Diaz <barbarad91@gmail.com>
2021-02-27 13:24:43 +0100

    Update user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,12 +1,29 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    username: String,
-    password: String
-}, {
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: ["STAFF", "ADMIN"],
+      default: "STAFF"
+    }
+  },
+  {
     timestamps: true
-})
+  }
+);
 
-const User = mongoose.model('User', userSchema)
-module.exports = User
+const User = mongoose.model("User", userSchema);
+module.exports = User;
