d5e1f2def53c35685efe4a535795559dd9aebbdd
Barbara Diaz <barbarad91@gmail.com>
2021-03-02 19:28:56 +0100

    Change booking service to class

--- server/models/booking.model.js
+++ server/models/booking.model.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
-const crypto = require("crypto");
+const crypto = require("crypto")
 
 const bookingSchema = new Schema(
   {
     name: {
       type: String,
       trim: true,
-      required: true
+      required: true,
     },
     dni: {
       type: String,
@@ -18,10 +18,10 @@ const bookingSchema = new Schema(
         validator: function (dniInput) {
           return /^\d{8}[A-HJ-NP-TV-Z]$|^[K,L,M,X,Y,Z]\d{7}[A-HJ-NP-TV-Z]$/gim.test(
             dniInput
-          );
+          )
         },
-        message: (props) => `${props.value} is not a valid dni number`
-      }
+        message: (props) => `${props.value} is not a valid dni number`,
+      },
     },
     email: {
       type: String,
@@ -29,74 +29,84 @@ const bookingSchema = new Schema(
       required: true,
       validate: {
         validator: function (emailInput) {
-          return /^([\w-\.\+]+@([\w-]+\.)+[\w-]{2,4})?$/gi.test(emailInput);
+          return /^([\w-\.\+]+@([\w-]+\.)+[\w-]{2,4})?$/gi.test(emailInput)
         },
-        message: (props) => `${props.value} is not a valid email`
-      }
+        message: (props) => `${props.value} is not a valid email`,
+      },
     },
     phoneNumber: {
       type: String,
       trim: true,
       validate: {
         validator: function (phoneInput) {
-          return /^\d{9}$/g.test(phoneInput);
-        }
-      }
+          return /^\d{9}$/g.test(phoneInput)
+        },
+      },
     },
     groupCode: {
       type: String,
-      trim: true
+      trim: true,
     },
 
     accomodation: {
       type: String,
-      enum: ["none", "Longbeach Surf House", "Cactus Salinas"],
+      enum: [
+        "none",
+        "surfcampLongbeach",
+        "surfcampCactus",
+        "surfcampJunior",
+        "surfcampSpecial",
+        "accommodationCactusLongbeach",
+        "accommodationCactusSingle",
+        "accommodationCactusDouble",
+        "accommodationCactusDeluxe",
+      ],
       default: "none",
-      required: true
+      required: true,
     },
 
     arrival: {
       date: {
         type: Date,
-        required: true
+        required: true,
       },
       transfer: {
         type: Boolean,
-        default: false
-      }
+        default: false,
+      },
     },
 
     departure: {
       date: {
         type: Date,
-        required: true
+        required: true,
       },
       transfer: {
         type: Boolean,
-        default: false
-      }
+        default: false,
+      },
     },
 
     firstTime: {
       type: Boolean,
       default: true,
-      required: true
+      required: true,
     },
 
     surfLevel: {
       type: String,
       enum: ["0", "0.5", "1", "1.5", "2", "noClass"],
-      required: true
+      required: true,
     },
 
     foodMenu: {
       type: String,
-      required: true
+      required: true,
     },
 
     discountCode: {
       type: String,
-      trim: true
+      trim: true,
     },
 
     additionalInfo: String,
@@ -106,49 +116,49 @@ const bookingSchema = new Schema(
     price: {
       type: Number,
       required: true,
-      min: 0
+      min: 0,
     },
 
     paid: {
       type: Boolean,
-      default: false
+      default: false,
     },
 
     status: {
       type: String,
       enum: ["pending", "accepted", "cancelled"],
-      default: "pending"
+      default: "pending",
     },
 
     bookingCode: {
       type: String,
       unique: true,
-      default: generateCode
-    }
+      default: generateCode,
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
-);
+)
 
 bookingSchema.pre("validate", function (next) {
   if (this.arrivalDate > this.departureDate) {
-    next(new Error("End date must be greater that Start date"));
+    next(new Error("End date must be greater that Start date"))
   } else if (this.arrivalDate < new Date()) {
-    next(new Error("Arrival date must be greater than current date"));
+    next(new Error("Arrival date must be greater than current date"))
   } else if (this.accomodation === "none" && this.surfLevel === "noClass") {
     next(
       new Error(
         "Invalid booking data: You must select either accomodation or classes"
       )
-    );
+    )
   } else {
-    next();
+    next()
   }
-});
+})
 
 function generateCode() {
-  return crypto.randomBytes(2).toString("hex");
+  return crypto.randomBytes(2).toString("hex")
 }
-const Booking = mongoose.model("Booking", bookingSchema);
-module.exports = Booking;
+const Booking = mongoose.model("Booking", bookingSchema)
+module.exports = Booking
