7ae6d164477e07238af397969d822c12b3960530
Barbara Diaz <barbarad91@gmail.com>
2021-03-03 20:30:48 +0100

    Update theme

--- client/src/components/theme/index.js
+++ client/src/components/theme/index.js
@@ -1,15 +1,16 @@
-import { createMuiTheme } from "@material-ui/core/styles";
-import { blue, amber } from "@material-ui/core/colors";
+import { createMuiTheme } from "@material-ui/core/styles"
+import { blue, amber } from "@material-ui/core/colors"
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: blue[500]
+      main: blue[500],
     },
     secondary: {
-      main: amber[200]
-    }
-  }
-});
+      main: amber[200],
+      light: amber[50],
+    },
+  },
+})
 
-export default theme;
+export default theme
