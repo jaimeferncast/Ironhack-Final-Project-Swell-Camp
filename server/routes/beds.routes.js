60f9fb11de3976063304d8033ce77691b1191bbb
Barbara Diaz <barbarad91@gmail.com>
2021-04-05 17:34:01 +0200

    Apply prettierrc styles

--- server/routes/beds.routes.js
+++ server/routes/beds.routes.js
@@ -1,37 +1,37 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const { checkIfLoggedIn } = require("../middlewares")
+const { checkIfLoggedIn } = require('../middlewares')
 
-const Bed = require("../models/bed.model")
+const Bed = require('../models/bed.model')
 
-router.get("/", checkIfLoggedIn, (_req, res) => {
+router.get('/', checkIfLoggedIn, (_req, res) => {
   Bed.find()
-    .select("code")
+    .select('code')
     .sort({ code: 1 })
     .then((response) => res.json(response))
-    .catch((err) => res.status(500).json({ code: 500, message: "Error fetching all beds", err }))
+    .catch((err) => res.status(500).json({ code: 500, message: 'Error fetching all beds', err }))
 })
 
-router.post("/new", checkIfLoggedIn, (req, res) => {
+router.post('/new', checkIfLoggedIn, (req, res) => {
   const bed = { ...req.body }
 
   Bed.create(bed)
     .then((response) => res.json(response))
-    .catch((err) => res.status(500).json({ code: 500, message: "Error saving bed", err }))
+    .catch((err) => res.status(500).json({ code: 500, message: 'Error saving bed', err }))
 })
 
-router.put("/:_id", checkIfLoggedIn, (req, res) => {
+router.put('/:_id', checkIfLoggedIn, (req, res) => {
   const { code, rateType } = req.body
 
   Bed.findByIdAndUpdate(req.params._id, { code, rateType }, { new: true })
     .then((response) => res.json(response))
-    .catch((err) => res.status(500).json({ code: 500, message: "Error editing bed", err }))
+    .catch((err) => res.status(500).json({ code: 500, message: 'Error editing bed', err }))
 })
 
-router.delete("/:_id", checkIfLoggedIn, (req, res) => {
+router.delete('/:_id', checkIfLoggedIn, (req, res) => {
   Bed.findOneAndDelete({ _id: req.params._id })
     .then((response) => res.json(response))
-    .catch((err) => res.status(500).json({ code: 500, message: "Error deleting bed", err }))
+    .catch((err) => res.status(500).json({ code: 500, message: 'Error deleting bed', err }))
 })
 
 module.exports = router
